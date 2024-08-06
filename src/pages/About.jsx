92697407b5f9c43c716a3bbb87fd281136cce665
Tyron_Thome <with.intopia@gmail.com>
2024-08-06 23:26:41 +0200

    experience company c out

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -80,12 +80,12 @@ const About = () => {
                   <h3 className="text-black text-xl font-poppins font-semibold">
                     {experience.title}
                   </h3>
-                  <p
+                  {/* <p
                     className="text-black-500 font-medium font-base"
                     style={{ margin: 0 }}
                   >
                     {experience.company_name}
-                  </p>
+                  </p> */}
                 </div>
 
                 <ul className="my-5 list-disc ml-5 space-y-2">
